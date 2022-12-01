a352bd63ea316256e2b8293d771d18f2d8891cf1
potter00 <omar.ignacio.uri@gmail.com>
2022-12-01 11:12:38 -0700

    nuevo logo, y header a incluedes

---
+++ includes/header.php
@@ -0,0 +1,15 @@
+        <header>
+        <div class="container">
+          <div class="header_logo">
+            <a href="/index.html">
+              <img src="media/logo_stonks.png" alt>
+            </a>
+          </div>
+          <nav>
+            <a href="#">TIENDA</a>
+            <a href="#">PERFIL</a>
+            <a href="#">BIBLIOTECA</a>
+            
+          </nav>
+        </div>
+        </header>
---
+++ index.php
@@ -0,0 +1,22 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Steam</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<?php 
+    include("includes/header.php");
+?>
+
+      <footer>
+        <h3>información</h3>
+
+      </footer>
+</body>
+</html>
