d04b1d36d9e058c8063113940b844dc05d2e8390
Jesus <jesus.canedo228508@potros.itson.edu.mx>
2022-12-08 21:32:01 -0600

    implementacion del css en juegos

--- listaJuegos.php
+++ listaJuegos.php
@@ -17,6 +17,7 @@ include("connections/conn_localhost.php");
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="styleTienda.css">
     <title>Lista de juegos</title>
 </head>
 
@@ -30,7 +31,8 @@ include("connections/conn_localhost.php");
     
 
     ?>
-    <div>
+    <br>
+    <div class="lista-juego">
         <h2>
             <?php echo $juegos[$i]['nombre'] ?>
         </h2>
